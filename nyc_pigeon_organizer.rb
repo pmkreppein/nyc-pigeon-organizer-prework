dce89bbab319b11862f8c5fe97cbe5c0a29a057f
Peter Kreppein <pmkreppein@gmail.com>
2018-09-28 22:12:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 
 def nyc_pigeon_organizer(data)
-  holder_object = {}
+holder_object = {}
 
-   data.each do |feature, values|
-     values.each do |value, pigeons|
-       pigeons.each do |pigeon|
-         holder_object[pigeon] ||= {}
-         holder_object[pigeon][feature] ||= []
-         holder_object[pigeon][feature] << value.to_s
-       end
-     end
-   end
-   holder_object
+data.each do |feature, values|
+values.each do |value, pigeons|
+  pigeons.each do |pigeon|
+  holder_object[pigeon] ||= {}
+  holder_object[pigeon][feature] ||= []
+  holder_object[pigeon][feature] << value.to_s
  end
+ end
+end
+holder_object
+end
